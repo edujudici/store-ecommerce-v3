1e94ca48ad94d367d46e3c7c001408c052d1416b
Eduardo Judici <ext.eduardo.judici@dafiti.com.br>
2023-03-14 00:48:51 -0300

    fix: security for envirments

--- src/Envoy.blade.php
+++ src/Envoy.blade.php
@@ -1,26 +1,14 @@
 @setup
     require __DIR__.'/vendor/autoload.php';
 
-    echo '1';
-
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-
-    echo '2';
 
     try {
         $dotenv->load();
-
-        echo '3';
-
         $dotenv->required(['DEPLOY_USER', 'DEPLOY_SERVER', 'DEPLOY_BRANCH', 'DEPLOY_REPO', 'DEPLOY_BASE_DIR'])->notEmpty();
-
-        echo '4';
-
     } catch( Exception $e)  {
         echo 'Environments error: ' . $e->getMessage();
     }
-
-    echo '5';
 
     $branch = env('DEPLOY_BRANCH');
     $repository = env('REPOSITORY');
@@ -29,18 +17,20 @@
     $release = date('YmdHis');
     $newReleaseDir = $releasesDir .'/'. $release;
 
-    echo '6';
+    function logMessage($message) {
+        return "echo '\033[32m" .$message. "\033[0m';\n";
+    }
 @endsetup
 
 @servers(['web' => env('DEPLOY_USER').'@'.env('DEPLOY_SERVER')])
 
 @story('deploy')
-    echo '7';
     testing_environments
 @endstory
 
 @task('testing_environments')
-    echo '8';
+    {{ logMessage("Testing environments") }}
+
     echo 'Testing deploy host: ' . env('DEPLOY_HOST')
     echo 'Testing branch: ' . env('BRANCH')
     echo 'Testing repository: ' . env('REPOSITORY')
