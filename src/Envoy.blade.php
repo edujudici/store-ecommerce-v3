e6c3ee3e5c96ffba3ee2c2308fc2ed0231ac8a8c
Eduardo Judici <ext.eduardo.judici@dafiti.com.br>
2023-03-15 00:31:08 -0300

    feat: deploy production

--- src/Envoy.blade.php
+++ src/Envoy.blade.php
@@ -3,36 +3,93 @@
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 
-    echo "load environments";
-
     try {
         $dotenv->load();
         $dotenv->required(['DEPLOY_USER', 'DEPLOY_SERVER', 'DEPLOY_BRANCH', 'DEPLOY_REPO', 'DEPLOY_BASE_DIR'])->notEmpty();
-    } catch( Exception $e)  {
-        echo 'Environments error: ' . $e->getMessage();
+    } catch ( Exception $e )  {
+        echo "Environments doesn`t exists or empty value, error: " . $e->getMessage();
     }
 
     $branch = env('DEPLOY_BRANCH');
     $repository = env('REPOSITORY');
-    $appDir = env('DEPLOY_BASE_DIR');
-    $releasesDir = $appDir . '/releases';
-    $release = date('YmdHis');
-    $newReleaseDir = $releasesDir .'/'. $release;
+    $baseDir = env('DEPLOY_BASE_DIR');
 
-    function logMessage($message) {
-        return "echo '\033[32m" .$message. "\033[0m';\n";
-    }
+    $releaseDir = $baseDir . '/releases';
+    $currentDir = $baseDir . '/current';
+    $release = date('YmdHis');
+    $currentReleaseDir = $releaseDir .'/'. $release;
 @endsetup
 
-@servers(['web' => env('DEPLOY_USER').'@'.env('DEPLOY_SERVER')])
+@servers(['prod' => env('DEPLOY_USER').'@'.env('DEPLOY_SERVER')])
 
 @story('deploy')
-    testing_environments
+    stop_containeres
+    clone_repository
+    update_symlinks
+    {{--  fix_permissions  --}}
+    start_containeres
+    run_composer
+    run_migrations
+    run_npm
 @endstory
 
-@task('testing_environments')
-    {{ logMessage("Testing environments") }}
-
-    echo {{ $branch }}
-
+@task('stop_containeres')
+    echo 'Stopping all actual services containeres'
+    cd {{ $currentDir }}
+    docker-compose -f docker-compose-production.yml down
 @endtask
+
+@task('clone_repository')
+    echo 'Cloning repository'
+    [ -d {{ $releaseDir }} ] || mkdir {{ $releaseDir }}
+    git clone --depth 1 --single-branch --branch {{ $branch }} {{ $repository }} {{ $currentReleaseDir }}
+    cd {{ $currentReleaseDir }}
+    git reset --hard {{ $commit }}
+@endtask
+
+@task('update_symlinks')
+    echo "Remove storage folder"
+    rm -rf {{ $currentReleaseDir }}/storage
+
+    echo 'Copy storage and env inside new version'
+    cp -r {{ $baseDir }}/shared/storage {{ $currentReleaseDir }}
+    cp {{ $baseDir }}/shared/.env {{ $currentReleaseDir }}
+
+    //echo 'Linking current release'
+    //ln -nfs {{ $currentReleaseDir }} {{ $baseDir }}/current
+@endtask
+
+{{--  @task('fix_permissions')
+    echo "Setting file and folder permissions"
+    cd {{ $currentDir }}
+    ./setup.sh
+@endtask  --}}
+
+@task('start_containeres')
+    echo 'Start all services containeres'
+    cd {{ $currentDir }}
+    docker-compose -f docker-compose-production.yml up -d --build
+    sleep 15
+@endtask
+
+@task('run_composer')
+    echo "Starting deployment ({{ $release }})"
+    cd {{ $currentDir }}
+    docker-compose -f docker-compose-production.yml exec app composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+@endtask
+
+@task('run_migrations')
+    echo "Running migrations..."
+    cd {{ $currentDir }}
+    docker-compose -f docker-compose-production.yml exec app php artisan migrate --force
+@endtask
+
+ @task('run_npm')
+    echo "Running npm..."
+    cd {{ $currentDir }}
+    docker compose -f docker-compose-production.yml run --rm --service-ports npm run production
+@endtask
+
+@finished
+    echo "Envoy deployment script finished.\r\n";
+@endfinished
