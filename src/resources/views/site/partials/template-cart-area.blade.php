f104885b24f496072feb018cc1ac9f2a657effcf
Eduardo Judici <ext.eduardo.judici@dafiti.com.br>
2023-04-30 21:49:53 -0300

    fix: cart product image

--- src/resources/views/site/partials/template-cart-area.blade.php
+++ src/resources/views/site/partials/template-cart-area.blade.php
@@ -30,7 +30,7 @@
                         <!-- PRODUCT -->
                         <div class="row">
                             <div class="col-12 col-sm-12 col-md-2 text-center">
-                                <img class="img-responsive" data-bind="attr: {src: image}" alt="prewiew" width="120" height="80">
+                                <img class="img-responsive" data-bind="attr: {src: image}" alt="prewiew" height="80">
                             </div>
                             <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
                                 <h4 class="product-name"><strong data-bind="text: title"></strong></h4>
