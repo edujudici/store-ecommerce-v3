74d9ebea58f61de0b6c4a8074418f2585745f366
Eduardo Judici <eduardo.judici@ciandt.com>
2024-09-06 17:19:07 -0300

    refactor: Reordering API Paths

--- src/app/Http/Controllers/API/MercadoLivre/MercadoLivreAnswerController.php
+++ src/app/Http/Controllers/API/MercadoLivre/MercadoLivreAnswerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers\API\MercadoLivre;
 
 use App\Http\Controllers\Controller;
 use App\Services\Seller\MercadoLivreAnswerService;
--- src/routes/web/storage-routes.php
+++ src/routes/web/storage-routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StorageController;
+use Illuminate\Support\Facades\Route;
 
 Route::name('storage.')->group(static function () {
     Route::get('storage/{filename?}', [StorageController::class, 'show'])->name('upload.show');
