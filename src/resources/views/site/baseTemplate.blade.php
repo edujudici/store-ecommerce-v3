073f371eda81f37282c16aee399a59da91f7d739
Eduardo Judici <eduardo.judici@ciandt.com>
2024-12-02 11:16:01 -0300

    refactor: Add Consentment Manager Platform

--- src/resources/views/site/baseTemplate.blade.php
+++ src/resources/views/site/baseTemplate.blade.php
@@ -1,61 +1,70 @@
 @extends('shared.masterpage')
 
 @section('custom_tag')
-@include('site.metaTag')
+    @include('site.metaTag')
 @endsection
 
 @section('custom_head')
-<!--
-        CSS
-        ============================================= -->
-<link rel="stylesheet" href="{{ asset('assets/site/css/linearicons.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/font-awesome.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/themify-icons.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/bootstrap.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/owl.carousel.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/ion.rangeSlider.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/ion.rangeSlider.skinFlat.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/magnific-popup.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/main.css') }}" />
-<link rel="stylesheet" href="{{ asset('assets/site/css/whatsapp.css') }}" />
+    <!--
+                    CSS
+                    ============================================= -->
+    <link rel="stylesheet" href="{{ asset('assets/site/css/linearicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/themify-icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/owl.carousel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/ion.rangeSlider.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/ion.rangeSlider.skinFlat.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/main.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/site/css/whatsapp.css') }}" />
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6Y3F1W6QVE"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+    @if (App::environment('production'))
+        {{-- Aviso de consentimento de uso de cookies --}}
+        <meta name="adopt-website-id" content="88e6b798-1416-4b0f-a7ec-89766e3213b8" />
+        <script src="//tag.goadopt.io/injector.js?website_code=88e6b798-1416-4b0f-a7ec-89766e3213b8" class="adopt-injector">
+        </script>
+    @endif
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6Y3F1W6QVE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'G-6Y3F1W6QVE');
-</script>
+    </script>
 
-<script type="text/javascript">
-    let company = {!! $company !!};
-</script>
+    <script type="text/javascript">
+        let company = {!! $company !!};
+    </script>
 
-@yield('specific_head')
-
+    @yield('specific_head')
 @endsection
 
 @section('maincontainer')
 
-<body>
+    <body>
 
-    @include('site.head')
+        @include('site.head')
 
-    @yield('content')
+        @yield('content')
 
-    @include('site.footer')
+        @include('site.footer')
 
-    <script src="{{ asset('assets/site/js/vendor/jquery-2.2.4.min.js') }}"></script>
-    <script src="{{ asset('assets/site/js/vendor/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/site/js/jquery.sticky.js') }}"></script>
-    <script src="{{ asset('assets/site/js/jquery.magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('assets/site/js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('assets/site/js/main.js') }}"></script>
-    <script src="{{asset('assets/site/js/jquery.maskedinput.js')}}"></script>
+        <script src="{{ asset('assets/site/js/vendor/jquery-2.2.4.min.js') }}"></script>
+        <script src="{{ asset('assets/site/js/vendor/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/site/js/jquery.sticky.js') }}"></script>
+        <script src="{{ asset('assets/site/js/jquery.magnific-popup.min.js') }}"></script>
+        <script src="{{ asset('assets/site/js/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('assets/site/js/main.js') }}"></script>
+        <script src="{{ asset('assets/site/js/jquery.maskedinput.js') }}"></script>
 
-    @yield('custom_script')
+        @yield('custom_script')
 
-</body>
+    </body>
 @endsection
